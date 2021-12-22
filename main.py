bc266b896167c4760f6e9f050e0a0e1094036626
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-21 22:27:33 -0800

    "created ball"

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15,  30, 30)
+
 while True:
     # Handles input
     for event in pygame.event.get():
@@ -16,6 +18,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    #updates the window
     pygame.display.flip()
     clock.tick(60)
 
