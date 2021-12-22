a05329da9023627cb1854f16ae30d02a11d5b9b1
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-21 22:59:29 -0800

    "refactoring code

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    #ball speed
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    #ball boundries
+    if ball.top <= 0 or ball.bottom >= screen_height: #verticle axis
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width: #horizontal axis
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x += -1
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -9,6 +25,7 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
+#draws our ball and paddles
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15,  30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
@@ -16,12 +33,19 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
+ball_speed_x = 7
+ball_speed_y = 7
+
 while True:
     # Handles input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball_animation()
+
+
 
     #visuals-background is put first. Last thing to render goes up top.
     screen.fill(bg_color)
