6b62f1f114a238f4b4c787a76062dca758475dd1
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-23 20:37:57 -0800

    "building game manager class"

--- main.py
+++ main.py
@@ -105,6 +105,33 @@ class Opponent(Block):
 
 
 class GameManager:
+    def __init__(self, ball_group, paddle_group):
+        self.player_score = 0
+        self.opponent_score = 0
+        self.ball_group = ball_group
+        self.paddle_group = paddle_group
+
+    def run_game(self):
+        self.paddle_group.draw(screen)
+        self.ball_group.draw(screen)
+
+        self.paddle_group.update(self.ball_group)
+        self.ball_group.update()
+        self.reset_ball()
+        self.draw_score()
+
+    def reset_ball(self):
+        if self.ball_group.rect.right >= screen_height:
+            self.opponent_score += 1
+            self.ball_group.sprite.reset_ball()
+        if self.ball_group.sprite.rect.left <= 0:
+            self.player_score += 1
+            self.ball_group.sprite.reset_ball()
+
+    def draw_score(self):
+        player_score = basic_font.render(str(self.player_score), True, accent_color)
+
+
 
 
 # set up
