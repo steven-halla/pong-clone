5042f394533890215f0f3617819532920a67d5aa
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-23 20:32:02 -0800

    "built opponent class"

--- main.py
+++ main.py
@@ -91,7 +91,18 @@ class Opponent(Block):
         self.speed = speed
 
     def update(self, ball_group):
-        if self.rect.top <
+        if self.rect.top < ball_group.sprite.rect.y:
+            self.rect.y += self.speed
+        if self.rect.bottom > ball_group.sprite.rect.y:
+            self.rect.y -= self.speed
+        self.constrain()
+
+    def constrain(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
+
 
 class GameManager:
 
